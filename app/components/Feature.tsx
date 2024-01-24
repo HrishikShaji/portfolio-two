3f3f0f2a954938e3434edda95c778a50c6e5301f
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-24 13:18:43 +0530

    fetures added

--- app/components/Feature.tsx
+++ app/components/Feature.tsx
@@ -3,36 +3,37 @@ import gsap from "gsap";
 import { useEffect, useState } from "react";
 import { BoxObj, Loop } from "./Loop";
 import { useMount } from "../hooks/useMount";
+import { Skill } from "./Skill";
 import { loopAnimation } from "../lib/utils";
+import { HorizontalBar } from "./HorizontalBar";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 const boxes: BoxObj[] = [
-	{ color: "hsl(0,0%,10%)", data: "Hi, I'm HRISHIK" },
 	{ color: "hsl(0,0%,20%)" },
 	{ color: "hsl(0,0%,30%)" },
 	{ color: "hsl(0,0%,40%)" },
+	{ color: "hsl(0,0%,50%)" },
 	{ color: "hsl(0,0%,60%)" },
 ];
 
-export const Hero = () => {
+export const Feature = () => {
 	const { isMounted } = useMount();
-
 	useEffect(() => {
 		gsap.registerPlugin(ScrollTrigger);
 		if (isMounted) {
-			const boxes = document.querySelectorAll(".hero-box");
+			const boxes = document.querySelectorAll(".feature-box");
 			boxes.forEach((element) => {
 				gsap.fromTo(
 					element,
 					{
-						scaleY: 0.5,
-						transformOrigin: "bottom",
+						scale: 0.75,
+						transformOrigin: "top right",
 					},
 					{
-						scaleY: 1,
+						scale: 1,
 						scrollTrigger: {
 							trigger: element,
-							start: "top bottom",
+							start: "top 80%",
 							end: "top 20%",
 							scrub: 2,
 						},
@@ -43,14 +44,15 @@ export const Hero = () => {
 		}
 		return () => window.removeEventListener("mousemove", loopAnimation);
 	}, [isMounted]);
+
 	return (
 		<div className="h-screen w-full ">
 			<Loop
-				boxName="hero"
+				boxName="feature"
 				number={4}
 				boxes={boxes}
-				paddingTop="40px"
-				paddingBottom="40px"
+				paddingTop="20px"
+				paddingBottom="20px"
 				paddingLeft="20px"
 				paddingRight="20px"
 			/>
--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,4 +1,5 @@
 import { About } from "./About";
+import { Feature } from "./Feature";
 import { Hero } from "./Hero";
 import { Projects } from "./Projects";
 import { Skills } from "./Skills";
@@ -8,6 +9,12 @@ export const Main = () => {
 		<div className="w-full h-full flex flex-col gap-10">
 			<Hero />
 			<About />
+
+			<div className="flex gap-2">
+				{Array.from({ length: 3 }).map((_, i) => (
+					<Feature key={i} />
+				))}
+			</div>
 			<Skills />
 			<Projects />
 		</div>
