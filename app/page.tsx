c1658aa8b3c9131d18b905724fed2940e98eaca8
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-25 23:49:49 +0530

    removed import error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { Hero } from "./components/Hero";
-import { About } from "./components/About";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
 import { Wrapper } from "./components/Wrapper";
 
 export default function Home() {
-	return (
-		<main className="p-5 bg-black">
-			<Wrapper />
-		</main>
-	);
+  return (
+    <main className="p-5 bg-black">
+      <Wrapper />
+    </main>
+  );
 }
